eaff109628d8c226a5907b5b1473e06414b1923f
leroy-netizen <leroythomas051@gmail.com>
2022-08-22 14:46:45 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    
+    def name=(dog_name)
+        @name = dog_name
+    end
 
+    def name
+        return @name
+    end
+
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
+
+    def breed
+        return @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+    def name=(person)
+        @name = person
+    end
 
+    def name
+        return @name
+    end
+
+    def job=(work)
+        @job = work
+    end
+
+    def job
+        return @job
+    end
 end
